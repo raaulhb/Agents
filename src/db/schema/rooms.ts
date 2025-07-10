ad33ddba9d8630361cb56dade13e7b9a8cfa0d79
Raul Barros <raulhbarros@outlook.com>
2025-07-10 15:32:27 +0100

    docs: add readme

--- src/db/schema/rooms.ts
+++ src/db/schema/rooms.ts
@@ -4,5 +4,5 @@ export const rooms = pgTable("rooms", {
   id: uuid().primaryKey().defaultRandom(),
   name: text().notNull(),
   description: text(),
-  createdAt: timestamp().defaultNow().notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
 });
