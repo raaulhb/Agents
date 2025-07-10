539d1123b4e8d0c324f6cebd386638d95c2e328c
Raul Barros <raulhbarros@outlook.com>
2025-07-10 15:32:57 +0100

    first setup

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,3 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
+
 export function App() {
-  return <div>Hello world</div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={<CreateRoom />}
+            index
+          />
+          {/** biome-ignore assist/source/useSortedAttributes: <explanation> */}
+          <Route
+            path="/room/:roomId"
+            element={<Room />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
