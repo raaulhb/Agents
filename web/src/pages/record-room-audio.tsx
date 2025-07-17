227cd5700304c963bf86863f1287e17cf36ddce0
Raul Barros <raulhbarros@outlook.com>
2025-07-17 13:59:57 +0100

    feat: salvando audio no frontend;

--- web/src/pages/record-room-audio.tsx
+++ web/src/pages/record-room-audio.tsx
@@ -1,15 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { useRef } from "react";
 import { useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
 
 const isRecordingSupported =
   !!navigator.mediaDevices &&
   typeof navigator.mediaDevices.getUserMedia === "function" &&
   typeof window.MediaRecorder === "function";
 
+type RoomParams = {
+  roomId: string;
+};
+
 export function RecordRoomAudio() {
+  const params = useParams<RoomParams>();
+
   const [isRecording, setIsRecording] = useState(false);
-  3;
   const recorder = useRef<MediaRecorder | null>(null);
 
   function stopRecording() {
@@ -19,6 +25,21 @@ export function RecordRoomAudio() {
       recorder.current.stop();
     }
   }
+  async function uploadAudio(audio: Blob) {
+    const formData = new FormData();
+    formData.append("file", audio, "audio.webm");
+
+    const response = await fetch(
+      `http://localhost:3333/rooms/${params.roomId}/audio`,
+      {
+        method: "POST",
+        body: formData,
+      }
+    );
+    const result = await response.json();
+    return result;
+  }
+
   async function startRecording() {
     if (!isRecordingSupported) {
       alert("Gravação de áudio não é suportada neste navegador.");
@@ -39,8 +60,7 @@ export function RecordRoomAudio() {
     });
     recorder.current.ondataavailable = (event) => {
       if (event.data.size > 0) {
-        // biome-ignore lint/suspicious/noConsole: <explanation>
-        console.log("Dados de áudio disponíveis:", event.data);
+        uploadAudio(event.data);
       }
     };
     recorder.current.onstart = () => {
@@ -53,6 +73,15 @@ export function RecordRoomAudio() {
       setIsRecording(false);
     };
     recorder.current.start();
+  }
+
+  if (!params.roomId) {
+    return (
+      <Navigate
+        replace
+        to="/"
+      />
+    );
   }
   return (
     <div className="h-screen flex items-center justify-center gap-3 flex-col">
